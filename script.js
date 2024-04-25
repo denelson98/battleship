96a60c0b29a1eb7fb193181744482cbc42b7d3ee
Davis <davismickens98@gmail.com>
2024-04-25 10:10:40 -0400

    script.js: added Ship methods

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ class Ship {
     constructor(){
         this.length = 0;
         this.hits = 0;
-        this.sunk = null;
+        this.sunk = false;
+    }
+
+    hit(){
+        if(this.isSunk()){
+            this.sunk = true;
+        }
+        this.hits++;
+    }
+
+    isSunk(){
+        return this.hits === this.length;
     }
 }
 
